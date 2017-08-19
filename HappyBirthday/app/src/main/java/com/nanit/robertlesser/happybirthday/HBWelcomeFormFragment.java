67a4a295ecd76cba2c27988500ffb17780321eaf
robert <rdlesser@gmail.com>
2017-08-19 15:29:40 +0300

    Created the welcome form fragment

---
+++ HappyBirthday/app/src/main/java/com/nanit/robertlesser/happybirthday/HBWelcomeFormFragment.java
@@ -0,0 +1,29 @@
+package com.nanit.robertlesser.happybirthday;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HBWelcomeFormFragment extends Fragment {
+
+
+    public HBWelcomeFormFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hb_welcom_form, container, false);
+    }
+
+}
