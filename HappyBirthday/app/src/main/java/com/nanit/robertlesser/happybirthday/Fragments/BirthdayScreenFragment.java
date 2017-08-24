f8657fa1fea91d6feec874b8a9b4c42d85ec0ed6
robert <rdlesser@gmail.com>
2017-08-24 15:59:37 +0300

    Renamed numbers pngs and added the background image and placeholder for the birthday screen

--- HappyBirthday/app/src/main/java/com/nanit/robertlesser/happybirthday/Fragments/BirthdayScreenFragment.java
+++ HappyBirthday/app/src/main/java/com/nanit/robertlesser/happybirthday/Fragments/BirthdayScreenFragment.java
@@ -1,11 +1,15 @@
 package com.nanit.robertlesser.happybirthday.Fragments;
 
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.nanit.robertlesser.happybirthday.R;
 
@@ -14,6 +18,9 @@ import com.nanit.robertlesser.happybirthday.R;
  */
 public class BirthdayScreenFragment extends Fragment {
 
+    private View detailsView;
+    private ImageView ivPictureImage;
+    private ImageView ivBackgroundImage;
 
     public static final String TAG = BirthdayScreenFragment.class.getSimpleName();
 
@@ -26,7 +33,18 @@ public class BirthdayScreenFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_birthday_screen, container, false);
+        detailsView = inflater.inflate(R.layout.fragment_birthday_screen, container, false);
+        findViewsById();
+
+//        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.default_place_holder_blue);
+//        ivPictureImage.setImageDrawable(drawable);
+
+        return detailsView;
+    }
+
+    private void findViewsById() {
+        ivPictureImage = detailsView.findViewById(R.id.picture_image);
+        ivBackgroundImage = detailsView.findViewById(R.id.background_image);
     }
 
 }
