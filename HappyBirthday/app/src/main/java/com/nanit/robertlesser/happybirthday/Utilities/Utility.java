50cf2305fe079f1312b822feb058c7aff5501281
robert <rdlesser@gmail.com>
2017-08-25 11:53:03 +0300

    Added some documentation to Utility

--- HappyBirthday/app/src/main/java/com/nanit/robertlesser/happybirthday/Utilities/Utility.java
+++ HappyBirthday/app/src/main/java/com/nanit/robertlesser/happybirthday/Utilities/Utility.java
@@ -20,6 +20,13 @@ public class Utility {
 
     public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 878;
 
+    /**
+     * Check if the permissions needed have already been granted to the app.
+     * If not - ask for the permissions that are missing
+     * @param context The context of the app (MainActivity)
+     * @param permissions A string array of the permissions needed
+     * @return True or false depending on whether or not the app has the needed permissions
+     */
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public static boolean checkPermission(final Context context, String[] permissions) {
 
@@ -56,6 +63,10 @@ public class Utility {
         return haveAllPermissions;
     }
 
+    /**
+     * Hide the soft keyboard from screen
+     * @param activity The activity from which to hide the keyboard from
+     */
     public static void hideSoftKeyboard(Activity activity) {
         InputMethodManager inputMethodManager =
                 (InputMethodManager) activity.getSystemService(
@@ -64,6 +75,12 @@ public class Utility {
                 activity.getCurrentFocus().getWindowToken(), 0);
     }
 
+    /**
+     * Save a string value in SharedPreferences
+     * @param context The context of the SharedPreferences instance
+     * @param fieldName The field to be used in the SharedPreferences to save the value in
+     * @param fieldValue The value of the field used to save
+     */
     public static void saveStringInPrefs(final Context context, String fieldName, String fieldValue) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(context.getPackageName(),
                 Context.MODE_PRIVATE);
@@ -72,6 +89,12 @@ public class Utility {
         editor.commit();
     }
 
+    /**
+     * Produce a random int in range [min, max]
+     * @param min The minimum value for the random range (inclusive)
+     * @param max The maximum value of the random range (inclusive)
+     * @return A random int in the range
+     */
     public static int randomIntInRange(int min, int max) {
         Random random = new Random();
         int randomInt = random.nextInt(max - min + 1) + min;
